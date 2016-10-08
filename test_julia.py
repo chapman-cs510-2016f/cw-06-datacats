9ce52a595f99f3f965449d53a93017cae2dd1636
Andrew D Nguyen <nguye695@mail.chapman.edu>
2016-10-08 01:14:17 +0000

    created test file for julia

---
+++ test_julia.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import Julia
+
+def test_julia():
+   f = Julia.julia( -1.037 + 0.17j )
+   assert f(-1.00 - 0.2j) == 0
+   assert f(-1.01 - 0.2j) == 20
+   assert f(-1.02 - 0.2j) == 13
+   assert f(-1.03 - 0.2j) == 10
